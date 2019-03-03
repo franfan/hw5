2b3dc835990913708c28fd612d23000216086ceb
franfan <47168232+franfan@users.noreply.github.com>
2019-03-02 22:51:48 -0500

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100);
+  noFill();
+   for (var i = 12; i < 390; i = i + 30){
+  for (var x = 20; x <390; x = x + 30){
+		 circle(x, i, 10,);
+		
+	}
+  }
+}
